278941492790a87b1880e392cc7edd6321f743da
Alex <github@sinn.io>
2025-03-16 12:50:01 +0200

    feat: improve layout in RulesBrowser and refine Sigma rule parsing

--- src/views/RulesBrowser.vue
+++ src/views/RulesBrowser.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="px-6 py-6">
+  <div class="">
     <div class="mx-auto">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 h-[calc(100vh-100px)]">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 h-screen md:pr-6">
         <SigmaRulesBrowser 
-          class="h-full border rounded-lg overflow-hidden shadow-sm" 
+          class="h-full overflow-hidden shadow-sm border-r"
           :initialRuleId="ruleId"
         />
-        <div class="md:col-span-2 h-full border rounded-lg overflow-hidden shadow-sm">
+        <div class="md:col-span-2 border md:rounded-lg overflow-hidden shadow-sm h-[calc(100vh-100px)] mt-6">
           <SigmaRuleViewer class="h-full" />
         </div>
       </div>
